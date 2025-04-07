685cbc718e0c400aed05c8a222b182224a8afe40
tonynahra <70106129+tonynahra@users.noreply.github.com>
2025-04-06 19:36:46 -0500

    Update cv.js

--- common/cv.js
+++ common/cv.js
@@ -3,17 +3,17 @@ function toggle(sec){
 	eSec = sec.nextElementSibling ;
 	secStatus = eSec.style.display == "none" ;
 	eSec.style.display = secStatus ? "block" : "none" ;
-	eSec.previousElementSibling.firstElementChild.src = secStatus ? "../img/opened.jpg" : "../img/closed.jpg" ;
+	eSec.previousElementSibling.firstElementChild.src = secStatus ? "../img/opened.png" : "../img/closed.png" ;
 }
 
 
 function toggleOLD(secName){
 	 if ( document.getElementById(secName).style.display==''){
 		 document.getElementById(secName).style.display = 'none' ;
-		 document.getElementById(secName+'Button').src = '../img/closed.jpg' ;
+		 document.getElementById(secName+'Button').src = '../img/closed.png' ;
 	 } else {
 		 document.getElementById(secName).style.display = '' ;
-		 document.getElementById(secName+'Button').src = '../img/opened.jpg' ;
+		 document.getElementById(secName+'Button').src = '../img/opened.png' ;
 	 }
 	secName = "msg" ;
 	document.getElementById(secName).style.visibility = 'hidden' ;
@@ -26,7 +26,7 @@ function toggleOLD(secName){
     var SUBs = document.querySelectorAll(".SUB");
     [].forEach.call(SUBs, function(SUB) {
 		SUB.style.display = Oper ? "none" : "block" ;
-		SUB.previousElementSibling.firstElementChild.src = Oper ? "../img/closed.jpg" : "../img/opened.jpg" ;
+		SUB.previousElementSibling.firstElementChild.src = Oper ? "../img/closed.png" : "../img/opened.png" ;
     });
 	document.getElementById("togAll").innerHTML = Oper ? "Expand All" : "Collapse All" ;
  }
