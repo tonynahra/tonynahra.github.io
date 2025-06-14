68eaad21257320ea670a14c8c90d7eb5abe40e3c
Tony Nahra <70106129+tonynahra@users.noreply.github.com>
2025-06-14 00:08:56 -0500

    Update album_V2.js

--- common/album_V2.js
+++ common/album_V2.js
@@ -44,7 +44,7 @@ window.onload = function ()
 		'click': function(){
 			// nextN = parseInt(document.getElementById('next').innerText)-1 ;	
 			nextN = -1 ;
-			for ( let i=parseInt(document.getElementById('next').innerText)-1 ; i > -1 ; i-- ) {
+			for ( let i=(parseInt(document.getElementById('next').innerText)-1) ; i > 0 ; i-- ) {
 				console.log( i + ">>>[" + document.querySelectorAll('div')[i].className + "]" ) ;
 				if ( document.querySelectorAll('div')[i].className != 'hide' ) {
 				   nextN = i ;
@@ -68,7 +68,7 @@ window.onload = function ()
 		'click': function(){
 			// nextN = parseInt(document.getElementById('next').innerText)+1 ;
 			nextN = -1 ;
-			for ( i=parseInt(document.getElementById('next').innerText)+1 ; i < imgTot ; i++ ) {
+			for ( i=(parseInt(document.getElementById('next').innerText)+1) ; i < imgTot ; i++ ) {
 				console.log( i + ">>>[" + document.querySelectorAll('div')[i].className + "]" ) ;
 				if ( document.querySelectorAll('div')[i].className != 'hide' ) {
 				   nextN = i ;
