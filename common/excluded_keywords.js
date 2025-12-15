f037aeff7bbaebc0808b5effcea2056d4f61cf08
Tony Nahra <70106129+tonynahra@users.noreply.github.com>
2025-12-15 13:41:12 -0600

    Create excluded_keywords.js

---
+++ common/excluded_keywords.js
@@ -0,0 +1,19 @@
+// excluded_keywords.js
+// Define an array of keywords to exclude from the dropdown list.
+// Use lowercase for all entries to ensure case-insensitive matching.
+
+const EXCLUDED_KEYWORDS = [
+    "water", 
+    "trees", 
+    "landscape", 
+    "nature", 
+    "city",
+    "view",
+    "photo",
+    "buildings",
+    "sky",
+    "clouds",
+    "rocks",
+    "winter",
+    "snow"
+];
